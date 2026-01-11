a60a4c3cbe499a1eca6960fa306b640a9c4a71ed
Maito17 <149741599+Maito17@users.noreply.github.com>
2026-01-11 17:28:00 -0500

    Fix import statement for OpenAI module

--- possitema/ia_ventas.py
+++ possitema/ia_ventas.py
@@ -1,4 +1,4 @@
-import openai
+from opneai import OpneAI
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
